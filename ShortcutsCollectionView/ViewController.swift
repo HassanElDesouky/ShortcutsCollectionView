621860b357e769d00784eefe6332adf7a09a5712
hassaneldesouky <hassaneldesouky@icloud.com>
2018-12-13 15:53:22 +0200

    Initial Commit

---
+++ ShortcutsCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShortcutsCollectionView
+//
+//  Created by Hassan El Desouky on 12/13/18.
+//  Copyright © 2018 Hassan El Desouky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
